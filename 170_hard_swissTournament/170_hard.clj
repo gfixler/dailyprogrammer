386b79899f26f140ba16ba6f21fe9b0dce798b0f
gfixler <gary@garyfixler.com>
2014-07-12 02:33:20 -0700

    Everything's done except the sorting and pairing
    Pairing is a tricky business, because there are two constraints; the
    Swiss tournament rules say the highest scoring players across all games
    always pair up (and in the event of a draw, a tie-breaker can fill in,
    such as adding up all of their opponent's scores against them), but the
    Danish variation states that players can only play each other once. As
    people on the thread¹ are finding out, these two restrictions, combined
    with greedy pairing while walking the list by highest score pairings,
    can lead to dead ends, where the final players by score pairing have
    already played one another. This is a hard one to solve.
    
    ¹ http://www.reddit.com/r/dailyprogrammer/comments/2afwlj/7112014_challenge_170_hard_swiss_tournament_with/

--- 170_hard_swissTournament/170_hard.clj
+++ 170_hard_swissTournament/170_hard.clj
@@ -1,26 +1,31 @@
 (defn make-player [called]
-  (atom {:name called :score [] :bonus [] :opponent []}))
+  {:name called :scores [] :opponents []})
+
+(defn tally [player]
+  (reduce + (map #(reduce + %) (:scores player))))
 
 (defn record-game [player score bonus opponent]
-  (swap! player #(merge-with into % {:score [score]
-                                     :bonus [bonus]
-                                     :opponent [(:name @opponent)]})))
+  (merge-with into player {:scores [[score bonus]]
+                           :opponents [(:name opponent)]}))
 
 (defn final-score [] (nth [[15 5] [10 10] [5 15]] (rand-int 3)))
 (defn play-bonus [] (- (rand-int 11) 5))
 
 (defn play-game [[player1 player2]]
   (let [[score1 score2] (final-score)]
-    (record-game player1 score1 (play-bonus) player2)
-    (record-game player2 score2 (play-bonus) player1)))
+    [(record-game player1 score1 (play-bonus) player2)
+     (record-game player2 score2 (play-bonus) player1)]))
 
-(defn tally [player]
-  (reduce + (:scores player)))
+(defn pair-players-by-scores [players]
+  (partition 2 (shuffle players)))
 
-(let [players (for [i (range 1 33)] (make-player (str "Player" i)))]
+(defn play-tourney [players]
   (loop [round 1
          matchups (partition 2 (shuffle players))]
-    (map play-game matchups)
-    (for [player players]
-      (println (:name @player) (:score @player)))))
+    (let [results (for [matchup matchups] (play-game matchup))]
+      (if (< round 6)
+        (recur (inc round) (pair-players-by-scores (flatten results)))
+        results))))
 
+(let [players (for [i (range 1 33)] (make-player (str "Player" i)))]
+  (play-tourney players))
