66d2c91079b37ba15df6bdb3362ef6b2e9b48a2e
gfixler <gary@garyfixler.com>
2014-07-12 01:18:48 -0700

    About to remove atoms

--- 170_hard_swissTournament/170_hard.clj
+++ 170_hard_swissTournament/170_hard.clj
@@ -1,21 +1,26 @@
-(defn get-winner [[[p1 p1s] [p2 p2s]]]
-  (if (> p1s p2s) p1 p2))
+(defn make-player [called]
+  (atom {:name called :score [] :bonus [] :opponent []}))
 
-(defn get-game-results []
-  (let [possible [[15 5] [10 10] [5 15]]
-        actual (rand-int 3)
-        [score1 score2] (nth possible actual)]
-    [(+ score1 (- (rand-int 11) 5))
-     (+ score2 (- (rand-int 11) 5))]))
+(defn record-game [player score bonus opponent]
+  (swap! player #(merge-with into % {:score [score]
+                                     :bonus [bonus]
+                                     :opponent [(:name @opponent)]})))
 
-(defn play-tourney [pairs]
-  (let [rounds (atom [])]
-    (loop [pairs (partition 2 (shuffle players))]
-      (let [results (for [pair pairs]
-                      (sort-by last (map vector pair (get-game-results))))]
-        (swap! rounds conj results)
-        (let [winners (map (comp first first) results)]
-          winners)))))
+(defn final-score [] (nth [[15 5] [10 10] [5 15]] (rand-int 3)))
+(defn play-bonus [] (- (rand-int 11) 5))
 
-(let [players (for [i (range 32)] (str "player" (inc i)))]
-  (play-tourney players))
+(defn play-game [[player1 player2]]
+  (let [[score1 score2] (final-score)]
+    (record-game player1 score1 (play-bonus) player2)
+    (record-game player2 score2 (play-bonus) player1)))
+
+(defn tally [player]
+  (reduce + (:scores player)))
+
+(let [players (for [i (range 1 33)] (make-player (str "Player" i)))]
+  (loop [round 1
+         matchups (partition 2 (shuffle players))]
+    (map play-game matchups)
+    (for [player players]
+      (println (:name @player) (:score @player)))))
+
