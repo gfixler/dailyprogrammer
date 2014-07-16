0027ed52b2dd01f537bbe92ccb89f479f8842cdd
gfixler <gary@garyfixler.com>
2014-07-15 18:46:06 -0700

    Correct output in 171_easy.hs

--- 171_easy_hexTo8x8Map/171_easy.hs
+++ 171_easy_hexTo8x8Map/171_easy.hs
@@ -1,18 +1,18 @@
 import Data.List
 
-str = foldl (++) ""
-smooshText s = str $ words s
-
 hexCharVal c = length $ takeWhile (/= c) "0123456789ABCDEF"
+
 hexDots c = ["    ","   *","  * ","  **",
              " *  "," * *"," ** "," ***",
              "*   ","*  *","* * ","* **",
              "**  ","** *","*** ","****"] !! (hexCharVal c)
-renderHexLine s = str (map hexDots $ smooshText s)
-renderHex s = str $ intersperse "\n" (map renderHexLine $ lines s)
 
-pic1 = "FF 81\nBD A5\nA5 BD\n81 FF"
-pic2 = "AA 55\nAA 55\nAA 55\nAA 55"
-pic3 = "3E 7F\nFC F8\nF8 FC\n7F 3E"
-pic4 = "93 93\n93 F3\nF3 93\n93 93"
+renderHexPair s = (hexDots $ head s) ++ (hexDots $ head (tail s))
+renderHexPic s = unlines (map renderHexPair $ words s)
+showHexPic = putStr . renderHexPic
+
+pic1 = "FF 81 BD A5 A5 BD 81 FF"
+pic2 = "AA 55 AA 55 AA 55 AA 55"
+pic3 = "3E 7F FC F8 F8 FC 7F 3E"
+pic4 = "93 93 93 F3 F3 93 93 93"
 
