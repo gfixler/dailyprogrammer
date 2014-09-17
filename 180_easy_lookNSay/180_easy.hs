e12981f783174005c990e1e72c4f4b1d59474356
gfixler <gary@garyfixler.com>
2014-09-16 18:45:16 -0700

    Get 180 easy working in Haskell

--- 180_easy_lookNSay/180_easy.hs
+++ 180_easy_lookNSay/180_easy.hs
@@ -1,12 +1,11 @@
 import Data.List
 
-strhead :: String -> String
-strhead = (:[]) . head
 
-strlen :: String -> String
-strlen = show . length
-
-lookNSay :: String -> [(String,String)]
-lookNSay s = zipWith (:) (map strlen g) (map strhead g)
-    where g = group s
+-- converts runs of chars in string to len + char
+-- e.g. "112223" => "213213" (i.e. "2 1s, 3 2s, 1 3")
+lookNSay :: String -> String
+lookNSay s = concatMap unpair $ zip lengs heads
+    where heads = map ((:[]) . head) $ group s
+          lengs = map (show . length) $ group s
+          unpair (a,b) = a ++ b
 
