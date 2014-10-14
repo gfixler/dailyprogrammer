a1743711458b670ba7f5f800dd8a195d1bf09069
gfixler <gary@garyfixler.com>
2014-10-13 20:07:40 -0700

    Add SmartStack data type

--- 184_easy_smartStackList/smrtstck.hs
+++ 184_easy_smartStackList/smrtstck.hs
@@ -3,6 +3,9 @@ module SmartStack where
 data SortL a = ESort | CSort a (SortL a) deriving Show
 data StackL a = EStack | CStack a (StackL a) deriving Show
 
+data SmartStack a = SmartStack { sorted  :: SortL a
+                               , stacked :: StackL a } deriving Show
+
 sortPush :: Ord a => SortL a -> a -> SortL a
 sortPush ESort n = CSort n ESort
 sortPush (CSort h t) n = (max h n) `CSort` (sortPush t (min h n))
