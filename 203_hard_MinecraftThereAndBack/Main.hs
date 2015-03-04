551f8eae51d8996303d126927a5b2da5f8090980
gfixler <gary@garyfixler.com>
2015-03-04 01:36:57 -0800

    Introduce, use XYZ, World types

--- 203_hard_MinecraftThereAndBack/Main.hs
+++ 203_hard_MinecraftThereAndBack/Main.hs
@@ -4,9 +4,11 @@ import System.Random (randomRIO)
 pick :: [a] -> IO a
 pick xs = randomRIO (0, length xs - 1) >>= return . (xs !!)
 
+type XYZ = (Int, Int, Int)
 data Block = Air | Dirt | Sand | Lava deriving (Show)
+type World = (XYZ, M.Map XYZ Block)
 
-newWorld :: Int -> Int -> Int -> M.Map (Int,Int,Int) Block
-newWorld l w h = M.fromList $ zip cells (repeat Air)
+newWorld :: XYZ -> World
+newWorld (l,w,h) = ((l,w,h), M.fromList $ zip cells (repeat Air))
     where cells = [(x,y,z) | x <- [0..l-1], y <- [0..w-1], z <- [0..h-1]]
 
