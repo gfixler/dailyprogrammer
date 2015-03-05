be1f6b0858ce544711b1609746dceda1a44b9ed0
gfixler <gary@garyfixler.com>
2015-03-05 02:37:50 -0800

    Add collapse1 and collapse functions
    It would be more efficient to walk a core (vertical stack of blocks)
    just one time, exchanging a column of Air with a column of Sand above
    it, or filling it in with Lava if the the column is capped on top with
    such (both of which would require keeping pointers while walking). This
    could be done in a single walk. However, it would be significantly more
    finicky to code up. This method has collapse1 walk the core, collapsing
    things just one level; an Air-Sand pairing (Sand on top) swap places,
    and an Air below a Lava becomes a Lava. This plays to the strengths of
    recursive list-walking in Haskell, and makes for small, simple, readable
    code. It also means that another function - collapse - must come through
    and call collapse1 over and over, until everything has finally settled.
    This is O(n×m), where n is the number of blocks being iterated, and m is
    the number of "settlings" that need to occur amongst them. I'm certain I
    can get this down to O(n), or similar, but I'll leave that for later.

--- 203_hard_MinecraftThereAndBack/Main.hs
+++ 203_hard_MinecraftThereAndBack/Main.hs
@@ -7,7 +7,7 @@ pick :: [a] -> IO a
 pick xs = randomRIO (0, length xs - 1) >>= return . (xs !!)
 
 type Cell = (Int, Int, Int)
-data Block = Air | Dirt | Sand | Lava deriving (Show)
+data Block = Air | Dirt | Sand | Lava deriving (Eq, Show)
 type World = M.Map Cell Block
 
 genCells :: Cell -> [Cell]
@@ -24,4 +24,13 @@ getBlock cells cell = M.lookup cell cells
 
 setBlock :: World -> Cell -> Block -> World
 setBlock w c b = M.adjust (const b) c w
+
+collapse1 :: [Block] -> [Block]
+collapse1 [] = []
+collapse1 (Air:Sand:xs) = Sand : Air : collapse1 xs
+collapse1 (Air:Lava:xs) = Lava : Lava : collapse1 xs
+collapse1 (x:xs) = x : collapse1 xs
+
+collapse :: [Block] -> [Block]
+collapse bs = let cs = collapse1 bs in if bs == cs then cs else collapse cs
 
