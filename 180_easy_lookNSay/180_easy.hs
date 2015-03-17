83f65d0d1201c742485f4dc69a7f5ea34d6d4580
gfixler <gary@garyfixler.com>
2015-03-17 02:28:18 -0700

    Simplify solution for 180 Easy: LookNSay

--- 180_easy_lookNSay/180_easy.hs
+++ 180_easy_lookNSay/180_easy.hs
@@ -4,8 +4,6 @@ import Data.List
 -- converts runs of chars in string to len + char
 -- e.g. "112223" => "213213" (i.e. "2 1s, 3 2s, 1 3")
 lookNSay :: String -> String
-lookNSay s = concatMap unpair $ zip lengs heads
-    where heads = map ((:[]) . head) $ group s
-          lengs = map (show . length) $ group s
-          unpair (a,b) = a ++ b
+lookNSay = concatMap foo . group
+    where foo = \x -> (show $ length x) ++ [head x]
 
