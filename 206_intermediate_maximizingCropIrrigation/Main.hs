83d546eb868842e1a370aa3716253f0907d65b41
gfixler <gary@garyfixler.com>
2015-03-18 16:22:44 -0700

    Simplify findPlants using gridCoords function

--- 206_intermediate_maximizingCropIrrigation/Main.hs
+++ 206_intermediate_maximizingCropIrrigation/Main.hs
@@ -1,13 +1,16 @@
-import qualified Data.Set as S
+import Data.Monoid (mconcat)
+import qualified Data.Set as S (Set, fromList)
 import System.Environment (getArgs)
 
 gridCoords :: Int -> Int -> [(Int, Int)]
 gridCoords x y = [(x,y) | y <- [0..succ x], x <- [0..succ y]]
 
-findPlants :: [String] -> S.Set (Int, Int)
-findPlants rs = S.fromList [(x,y) | (cs,y) <- zip rs [0..], (p,x) <- zip cs [0..], p == 'x']
+findPlants :: Int -> Int -> String -> S.Set (Int, Int)
+findPlants w h xs = S.fromList [c | (c,x) <- zip cs xs, x == 'x']
+    where cs = gridCoords w h
 
 main = do
-    (h:w:r:rs) <- getArgs
-    print $ findPlants rs
+    (h:w:r:xs) <- getArgs
+    let plants = findPlants (read w) (read h) (mconcat xs)
+    print plants
 
