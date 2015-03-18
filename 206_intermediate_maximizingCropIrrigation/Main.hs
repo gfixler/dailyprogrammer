1fab6bbc4e554e90c400183b82de1775c1e85102
gfixler <gary@garyfixler.com>
2015-03-18 16:12:57 -0700

    Add gridCoords function
    This just generates XY pairs for the given width and height.

--- 206_intermediate_maximizingCropIrrigation/Main.hs
+++ 206_intermediate_maximizingCropIrrigation/Main.hs
@@ -2,6 +2,9 @@ import qualified Data.Map as M
 import qualified Data.Set as S
 import System.Environment
 
+gridCoords :: Int -> Int -> [(Int, Int)]
+gridCoords x y = [(x,y) | y <- [0..succ x], x <- [0..succ y]]
+
 findPlants :: [String] -> S.Set (Int, Int)
 findPlants rs = S.fromList [(x,y) | (cs,y) <- zip rs [0..], (p,x) <- zip cs [0..], p == 'x']
 
