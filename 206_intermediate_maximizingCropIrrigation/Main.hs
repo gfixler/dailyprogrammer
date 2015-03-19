dc0eae58688c3c287f39da25662396f6c07279b0
gfixler <gary@garyfixler.com>
2015-03-18 19:00:50 -0700

    Rename gridCoords -> gridPoints

--- 206_intermediate_maximizingCropIrrigation/Main.hs
+++ 206_intermediate_maximizingCropIrrigation/Main.hs
@@ -2,12 +2,12 @@ import Data.Monoid (mconcat)
 import qualified Data.Set as S (Set, fromList)
 import System.Environment (getArgs)
 
-gridCoords :: Int -> Int -> [(Int, Int)]
-gridCoords x y = [(x,y) | y <- [0..succ x], x <- [0..succ y]]
+gridPoints :: Int -> Int -> [(Int, Int)]
+gridPoints x y = [(x,y) | y <- [0..succ x], x <- [0..succ y]]
 
 findPlants :: Int -> Int -> String -> S.Set (Int, Int)
 findPlants w h xs = S.fromList [c | (c,x) <- zip cs xs, x == 'x']
-    where cs = gridCoords w h
+    where cs = gridPoints w h
 
 main = do
     (h:w:r:xs) <- getArgs
