183cf2081e15299e8659f45cbc77f73046d0d282
gfixler <gary@garyfixler.com>
2015-03-25 15:58:26 -0700

    Add Strand, End, and RE (restriction enzyme) types

--- 207_intermediate_DNARestrictionEnzymes/Main.hs
+++ 207_intermediate_DNARestrictionEnzymes/Main.hs
@@ -2,6 +2,10 @@ import Data.Char (toLower)
 import System.Environment (getArgs)
 import System.IO (readFile)
 
+newtype Strand = Strand String deriving (Show)
+data End = Sticky | Blunt
+data RE = RE String End String String
+
 main = do
     [file] <- getArgs
     contents <- readFile file
