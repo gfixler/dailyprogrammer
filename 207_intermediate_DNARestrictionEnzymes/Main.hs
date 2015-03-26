5fafa0ee0de7d5dad7810af1e7fb282dafcdcb53
gfixler <gary@garyfixler.com>
2015-03-25 18:41:56 -0700

    Rename toRecSeq to makeCutter
    Names are hard.

--- 207_intermediate_DNARestrictionEnzymes/Main.hs
+++ 207_intermediate_DNARestrictionEnzymes/Main.hs
@@ -1,4 +1,4 @@
-module Main (main, End, filterToStrand, toRecSeq, toRECutter) where
+module Main (main, End, filterToStrand, makeCutter, toRECutter) where
 
 import Data.Char (toLower)
 import Data.List (findIndices)
@@ -12,14 +12,14 @@ data RE = RE String End Strand Strand deriving (Show)
 filterToStrand :: String -> Strand
 filterToStrand = Strand . filter (`elem` "acgt") . map toLower
 
-toRecSeq :: String -> Either String (Strand, Strand)
-toRecSeq s = case findIndices (== '^') s of
+makeCutter :: String -> Either String (Strand, Strand)
+makeCutter s = case findIndices (== '^') s of
                  []  -> Left ("No '^' in cut sequence " ++ s)
                  [n] -> Right (filterToStrand $ reverse $ take n s, filterToStrand $ drop (n+1) s)
                  _   -> Left ("More than 1 '^' in cut sequence " ++ s)
 
 toRECutter :: String -> End -> String -> Either String RE
-toRECutter n e s = case toRecSeq s of
+toRECutter n e s = case makeCutter s of
                  Right (l, r) -> Right $ RE n e l r
                  Left x       -> Left x
 
