89f208455f3afb611d0a3ea137dc54cb4f0eaec8
gfixler <gary@garyfixler.com>
2015-03-25 17:08:41 -0700

    Add toRecSeq function
    I'm breaking out the oft-overlooked [by me] Either type for this one.

--- 207_intermediate_DNARestrictionEnzymes/Main.hs
+++ 207_intermediate_DNARestrictionEnzymes/Main.hs
@@ -1,6 +1,7 @@
 module Main (main, End, toStrand) where
 
 import Data.Char (toLower)
+import Data.List (findIndices)
 import System.Environment (getArgs)
 import System.IO (readFile)
 
@@ -10,6 +11,12 @@ data RE = RE String End Strand Strand deriving (Show)
 
 toStrand :: String -> Strand
 toStrand = Strand . filter (`elem` "acgt") . map toLower
+
+toRecSeq :: String -> Either String (Strand, Strand)
+toRecSeq s = case findIndices (== '^') s of
+                 []  -> Left ("No '^' in cut sequence " ++ s)
+                 [n] -> Right (toStrand $ reverse $ take n s, toStrand $ drop (n+1) s)
+                 _   -> Left ("More than 1 '^' in cut sequence " ++ s)
 
 toRE :: String -> End -> String -> RE
 toRE n e s = RE n e l r
