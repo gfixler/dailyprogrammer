0854a67f2cec16e80e1e914f2580b44f2239d93a
gfixler <gary@garyfixler.com>
2015-04-01 10:35:31 -0700

    Rename radGrad to radGradVal

--- 208_intermediate_ASCIIGradientGenerator/Main.hs
+++ 208_intermediate_ASCIIGradientGenerator/Main.hs
@@ -6,7 +6,7 @@ hypot2D (x1,y1) (x2,y2) = sqrt (x^2 + y^2)
     where x = x2 - x1
           y = y2 - y1
 
-radGrad :: (Floating a, Ord a) => (a, a) -> a -> a -> (a, a) -> a
-radGrad (cx,cy) ri ro (x,y) = clamp ri ro d
+radGradVal :: (Floating a, Ord a) => (a, a) -> a -> a -> (a, a) -> a
+radGradVal (cx,cy) ri ro (x,y) = clamp ri ro d
     where d = hypot2D (cx,cy) (x,y)
 
