3eacedb170adadc337c26fab0438055f97ee593f
gfixler <gary@garyfixler.com>
2015-04-01 13:56:04 -0700

    Refactor radGradVal function to use clampSelect

--- 208_intermediate_ASCIIGradientGenerator/Main.hs
+++ 208_intermediate_ASCIIGradientGenerator/Main.hs
@@ -11,7 +11,7 @@ hypot2D (x1,y1) (x2,y2) = sqrt (x^2 + y^2)
     where x = x2 - x1
           y = y2 - y1
 
-radGradVal :: (Floating a, Ord a) => (a, a) -> a -> a -> (a, a) -> a
-radGradVal (cx,cy) ri ro (x,y) = clamp ri ro d
+radGradVal :: (Floating a, RealFrac a) => [b] -> (a, a) -> a -> a -> (a, a) -> b
+radGradVal cs (cx,cy) ri ro (x,y) = clampSelect cs ri ro d
     where d = hypot2D (cx,cy) (x,y)
 
