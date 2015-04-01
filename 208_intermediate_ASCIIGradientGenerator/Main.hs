e8342c525933050d7bd17ce4f8ddc14ebbf53e74
gfixler <gary@garyfixler.com>
2015-04-01 09:56:15 -0700

    Add hypot2D function
    This is the 2D hypoteneuse function.

--- 208_intermediate_ASCIIGradientGenerator/Main.hs
+++ 208_intermediate_ASCIIGradientGenerator/Main.hs
@@ -1,3 +1,8 @@
 clamp :: Ord a => a -> a -> a -> a
 clamp l h v = min h (max l v)
 
+hypot2D :: Floating a => a -> a -> a -> a -> a
+hypot2D x1 y1 x2 y2 = sqrt (x^2 + y^2)
+    where x = x2 - x1
+          y = y2 - y1
+
