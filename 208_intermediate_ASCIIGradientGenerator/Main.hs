e29f9de178cb904eeb490897949a6e42cf9df890
gfixler <gary@garyfixler.com>
2015-04-01 19:01:03 -0700

    Add mode to loop calls, including recursive calls
    We're not using the mode yet, but at least we're passing it through.

--- 208_intermediate_ASCIIGradientGenerator/Main.hs
+++ 208_intermediate_ASCIIGradientGenerator/Main.hs
@@ -47,32 +47,32 @@ main = do
     hSetBuffering stdin NoBuffering
     hSetBuffering stdout NoBuffering
     hSetEcho stdout False
-    loop w
+    loop Window w
 
-loop a@(World w h x y i o s) = do
+loop mode a@(World w h x y i o s) = do
     print a
     k <- getChar
     case k of
         'q' -> return ()
-        'f' -> loop $ World (succ w) h x y i o s
-        'b' -> loop $ World (max 1 (pred w)) h x y i o s
-        'u' -> loop $ World w (max 1 (pred h)) x y i o s
-        'd' -> loop $ World w (succ h) x y i o s
-        'h' -> loop $ World w h (pred x) y i o s
-        'l' -> loop $ World w h (succ x) y i o s
-        'j' -> loop $ World w h x (succ y) i o s
-        'k' -> loop $ World w h x (pred y) i o s
-        ']' -> loop $ World w h x y (succ i) o s
-        '[' -> loop $ World w h x y (max 0 (pred i)) o s
-        '>' -> loop $ World w h x y i (succ o) s
-        '<' -> loop $ World w h x y i (max 0 (pred o)) s
+        'f' -> loop mode $ World (succ w) h x y i o s
+        'b' -> loop mode $ World (max 1 (pred w)) h x y i o s
+        'u' -> loop mode $ World w (max 1 (pred h)) x y i o s
+        'd' -> loop mode $ World w (succ h) x y i o s
+        'h' -> loop mode $ World w h (pred x) y i o s
+        'l' -> loop mode $ World w h (succ x) y i o s
+        'j' -> loop mode $ World w h x (succ y) i o s
+        'k' -> loop mode $ World w h x (pred y) i o s
+        ']' -> loop mode $ World w h x y (succ i) o s
+        '[' -> loop mode $ World w h x y (max 0 (pred i)) o s
+        '>' -> loop mode $ World w h x y i (succ o) s
+        '<' -> loop mode $ World w h x y i (max 0 (pred o)) s
         's' -> do
             putStr "string: "
             hSetEcho stdout True
             s' <- getLine
             hSetEcho stdout False
-            loop $ World w h x y i o s'
+            loop mode $ World w h x y i o s'
         c   -> do
             putStrLn $ show c
-            loop $ World w h x y i o s
+            loop mode $ World w h x y i o s
 
