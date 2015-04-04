1c8819bb20accde3c37ed465ff5294266e83743f
gfixler <gary@garyfixler.com>
2015-04-03 19:57:58 -0700

    Take a file path in main and parse it

--- 208_hard_theUniversalMachine/Main.hs
+++ 208_hard_theUniversalMachine/Main.hs
@@ -2,6 +2,10 @@ module Main (main) where
 
 import Parse (parseTransition, parse)
 import Turing ()
+import System.Environment (getArgs)
 
-main = undefined
+main = do
+    [f] <- getArgs
+    d   <- parse $ readFile f
+    print d
 
