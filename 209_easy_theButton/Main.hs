da08842690cf02e540940fa64f8df79186689f90
gfixler <gary@garyfixler.com>
2015-04-06 02:54:02 -0700

    Remove utpairs temp data

--- 209_easy_theButton/Main.hs
+++ 209_easy_theButton/Main.hs
@@ -2,8 +2,6 @@ import Control.Monad (forM)
 import Data.List (sortBy)
 import Data.Ord (comparing)
 
-utpairs = [("UserA", 41.04), ("UserB", 7.06), ("UserC", 20.63), ("UserD", 54.28), ("UserE", 12.59), ("UserF", 31.17), ("UserG", 63.04)]
-
 getFlairs :: (RealFrac a, Integral b) => [(String, a)] -> [(String, b)]
 getFlairs xs = r 0 (sortBy (comparing snd) xs)
     where r _ [] = []
