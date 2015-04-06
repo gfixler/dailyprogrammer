b59134c5b9d535a608cb46798fb15f774cf80ce6
gfixler <gary@garyfixler.com>
2015-04-06 02:49:45 -0700

    Make imports explicit

--- 209_easy_theButton/Main.hs
+++ 209_easy_theButton/Main.hs
@@ -1,5 +1,5 @@
-import Data.List
-import Data.Ord
+import Data.List (sortBy)
+import Data.Ord (comparing)
 
 utpairs = [("UserA", 41.04), ("UserB", 7.06), ("UserC", 20.63), ("UserD", 54.28), ("UserE", 12.59), ("UserF", 31.17), ("UserG", 63.04)]
 
