47154412de93cc3f7037e5d1b1e464a89979911b
gfixler <gary@garyfixler.com>
2015-04-06 02:50:22 -0700

    Simplify names in getFlairs function

--- 209_easy_theButton/Main.hs
+++ 209_easy_theButton/Main.hs
@@ -4,7 +4,7 @@ import Data.Ord (comparing)
 utpairs = [("UserA", 41.04), ("UserB", 7.06), ("UserC", 20.63), ("UserD", 54.28), ("UserE", 12.59), ("UserF", 31.17), ("UserG", 63.04)]
 
 getFlairs :: (RealFrac a, Integral b) => [(String, a)] -> [(String, b)]
-getFlairs xs = click 0 (sortBy (comparing snd) xs)
-    where click _ [] = []
-          click o ((u,t):xs) = (u,floor $ 60-(t-o)) : click t xs
+getFlairs xs = r 0 (sortBy (comparing snd) xs)
+    where r _ [] = []
+          r p ((u,t):xs) = (u,floor $ 60-(t-p)) : r t xs
 
