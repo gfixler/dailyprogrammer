71adb8bc96e778759c183e59d7183ae1e4efd724
gfixler <gary@garyfixler.com>
2015-04-07 19:41:23 -0700

    Add volume function; test out in main

--- 200_hard_boxInABox/Main.hs
+++ 200_hard_boxInABox/Main.hs
@@ -15,6 +15,9 @@ readInts = map read . words
 readXYZ :: String -> (Int,Int,Int)
 readXYZ s = let (x:y:z:[]) = readInts s in (x,y,z)
 
+volume :: Num a => a -> a -> a -> a
+volume x y z = x * y * z
+
 main = do
     s <- getLine
     getLine
@@ -23,4 +26,5 @@ main = do
         ls' = map readXYZ ls
     print s'
     print ls'
+    print $ map (\(x,y,z) -> volume x y z) ls'
 
