d51725186c48bd3feee320a2f555160b1c6c8d4a
gfixler <gary@garyfixler.com>
2015-04-08 22:06:59 -0700

    Remove boxes, box functions
    I'm heading a different way for the moment.

--- 209_intermediate_packingASentenceInABox/Main.hs
+++ 209_intermediate_packingASentenceInABox/Main.hs
@@ -1,11 +1,5 @@
 import System.Random (getStdRandom, randomR)
 
-boxes :: Int -> (Int, Int)
-boxes x = head $ reverse [(m,n) | m <- [1..x], n <- [m..x], m*n == x]
-
-box :: Int -> (Int, Int)
-box x = head $ reverse [(m,n) | n <- [1..x], m <- [n..x], m*n == x]
-
 choose :: [a] -> IO a
 choose xs = do
     i <- getStdRandom (randomR (0, length xs - 1))
