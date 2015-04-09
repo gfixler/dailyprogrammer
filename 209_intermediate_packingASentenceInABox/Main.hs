5e32b00efd15858b7d51333d0104fbf6de4a7581
gfixler <gary@garyfixler.com>
2015-04-08 19:29:41 -0700

    Add box function
    This returns the closest pair of factors to the input's root.

--- 209_intermediate_packingASentenceInABox/Main.hs
+++ 209_intermediate_packingASentenceInABox/Main.hs
@@ -1,3 +1,6 @@
 boxes :: Int -> (Int, Int)
 boxes x = head $ reverse [(m,n) | m <- [1..x], n <- [m..x], m*n == x]
 
+box :: Int -> (Int, Int)
+box x = head $ reverse [(m,n) | n <- [1..x], m <- [n..x], m*n == x]
+
