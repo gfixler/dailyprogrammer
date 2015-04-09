fd8e78d4b6cebcfaea0b6a1841409d3375157b7c
gfixler <gary@garyfixler.com>
2015-04-08 22:12:25 -0700

    Add expandPack function
    This is a purely functional way of expanding a 2D grid of folded-up
    elements (e.g. something like a text spiral) such that each change -
    each "Expansion" - adds a whole new side either horizontally or
    vertically from the most recently added corner.

--- 209_intermediate_packingASentenceInABox/Main.hs
+++ 209_intermediate_packingASentenceInABox/Main.hs
@@ -12,3 +12,15 @@ data ExBox = ExBox (Int, Int) (Int, Int) Corner [(Int, Int)] deriving (Show)
 
 exBox corner = ExBox (1,1) (0,0) corner [(0,0)]
 
+expandPack :: Expansion -> ExBox -> ExBox
+expandPack e (ExBox (w,h) (x,y) c ps) =
+    case (e,c) of
+        (H, TL) -> ExBox (w+1,h) (x-1,y+h-1) BL (ps ++ zip (repeat (x-1)) [y..(y+h-1)])
+        (V, TL) -> ExBox (w,h+1) (x+w-1,y-1) TR (ps ++ zip [x..(x+w-1)] (repeat (y-1)))
+        (H, TR) -> ExBox (w+1,h) (x+1,y+h-1) BR (ps ++ zip (repeat (x+1)) [y..(y+h-1)])
+        (V, TR) -> ExBox (w,h+1) (x+w-1,y-1) TL (ps ++ zip (reverse [x..(x+w-1)]) (repeat (y-1)))
+        (H, BL) -> ExBox (w+1,h) (x-1,y+h-1) TL (ps ++ zip (repeat (x-1)) (reverse [y..(y+h-1)]))
+        (V, BL) -> ExBox (w,h+1) (x+w-1,y+1) BR (ps ++ zip [x..(x+w-1)] (repeat (y+1)))
+        (H, BR) -> ExBox (w+1,h) (x+1,y+h-1) TR (ps ++ zip (repeat (x+1)) (reverse [y..(y+h-1)]))
+        (V, BR) -> ExBox (w,h+1) (x+w-1,y+1) BL (ps ++ zip (reverse [x..(x+w-1)]) (repeat (y+1)))
+
