6e2d7728a72bbeb310d1b8ea0fa58ba2c74b9210
gfixler <gary@garyfixler.com>
2015-04-08 22:08:12 -0700

    Add Corner, Expansion data types
    Corner will help keep track of which corner the cursor is in.

--- 209_intermediate_packingASentenceInABox/Main.hs
+++ 209_intermediate_packingASentenceInABox/Main.hs
@@ -5,3 +5,6 @@ choose xs = do
     i <- getStdRandom (randomR (0, length xs - 1))
     return $ xs !! i
 
+data Corner    = TL | TR | BL | BR deriving (Eq, Show)
+data Expansion = H | V deriving (Eq, Show)
+
