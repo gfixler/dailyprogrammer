730caa893e0d8dc38d17a5679eb3fe8528cf90dd
gfixler <gary@garyfixler.com>
2015-04-15 15:26:29 -0700

    Improve toPixmap
    The input format is now [[(Int,Int,Int)]]. The width and height are
    determined from the input, and "rag right" edges are trimmed.

--- 210_intermediate_drawingAGradient/Main.hs
+++ 210_intermediate_drawingAGradient/Main.hs
@@ -1,12 +1,17 @@
+toPixmap :: [[(Int,Int,Int)]] -> String
+toPixmap xss = header ++ image xss
+    where header = "P3\n" ++ show width ++ " " ++ show (length xss) ++ " 255\n"
+          width = minimum $ map length xss
+          pixel (a,b,c) = unwords $ map show [a,b,c]
+          line = unwords . map pixel
+          image = unwords . map line
+
 spacedInts :: String -> [Int]
 spacedInts = map read . words
-
-toPixmap :: Int -> Int -> [(Int,Int,Int)] -> String
-toPixmap w h ps = "P3\n" ++ show w ++ " " ++ show h ++ "\n"
 
 main = do
     [w,h] <- fmap spacedInts $ getLine
     [ar,ag,ab] <- fmap spacedInts $ getLine
     [br,bg,bb] <- fmap spacedInts $ getLine
-    putStr (toPixmap w h [])
+    putStrLn "done"
 
