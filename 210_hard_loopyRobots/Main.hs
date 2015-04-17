b67ec88fe457cc83936e2fe6af5b4b2300bf9f84
gfixler <gary@garyfixler.com>
2015-04-16 20:06:35 -0700

    Add parseCommands function
    This isn't total, and fails on all but a string of only "SLR" chars.

--- 210_hard_loopyRobots/Main.hs
+++ 210_hard_loopyRobots/Main.hs
@@ -2,3 +2,9 @@ data Facing = N | S | E | W      deriving (Show)
 data Cmd    = Fwd | TL | TR      deriving (Show)
 data Bot    = Bot Int Int Facing deriving (Show)
 
+parseCommands :: String -> [Cmd]
+parseCommands = map parseCommand
+    where parseCommand x = case x of 'S' -> Fwd
+                                     'L' -> TL
+                                     'R' -> TR
+
