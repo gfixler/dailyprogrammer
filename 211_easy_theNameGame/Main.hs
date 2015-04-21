169d365883abb51868271c49bd27c63692a4cc1e
gfixler <gary@garyfixler.com>
2015-04-21 01:50:11 -0700

    Try different take on 211 Easy game function

--- 211_easy_theNameGame/Main.hs
+++ 211_easy_theNameGame/Main.hs
@@ -6,8 +6,9 @@ bfm c (x:xs) | c == x = c:'o':'-':xs
 bfm c (_:xs) = toLower c:'o':' ':c:xs
 
 game :: String -> String
-game name = concat [name,", ",name," ",bfm 'B' name,",\nBonana fanna "
-                   ,bfm 'F' name,",\nFee fy ",bfm 'M' name,",\n",name,"!"]
+game name = concat $ zipWith (++) names words
+    where words = [", "," ",",\nBonana fanna ",",\nFee fy ",",\n","!"]
+          names = map (flip ($) name) [id,id,bfm 'B',bfm 'F',bfm 'M',id]
 
 main = getLine >>= putStrLn . game . init
 
