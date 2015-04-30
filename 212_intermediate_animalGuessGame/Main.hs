186dbf93c99660062ffe2caf3ee57c6ad67ab5a2
gfixler <gary@garyfixler.com>
2015-04-30 04:40:04 -0700

    Implement goLeft, goRight, goUp functions
    At last I can walk around the Tree.

--- 212_intermediate_animalGuessGame/Main.hs
+++ 212_intermediate_animalGuessGame/Main.hs
@@ -10,3 +10,16 @@ type TreeZipper a b = (Tree a b, Side, [Crumb a b])
 zipperTree :: Tree a b -> TreeZipper a b
 zipperTree t = (t, T, [])
 
+goLeft :: TreeZipper a b -> Maybe (TreeZipper a b)
+goLeft ((Branch x l r), s, cs) = Just (l, L, (x, s, r) : cs)
+goLeft _ = Nothing
+
+goRight :: TreeZipper a b -> Maybe (TreeZipper a b)
+goRight ((Branch x l r), s, cs) = Just (r, R, (x, s, l) : cs)
+goRight _ = Nothing
+
+goUp :: TreeZipper a b -> Maybe (TreeZipper a b)
+goUp (l, L, ((x,s,r):cs)) = Just (Branch x l r,s,cs)
+goUp (r, R, ((x,s,l):cs)) = Just (Branch x l r,s,cs)
+goUp _ = Nothing
+
