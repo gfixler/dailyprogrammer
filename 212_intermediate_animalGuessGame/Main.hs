43ee8d705bef10e0cf3e2226bcc1e936dca090d5
gfixler <gary@garyfixler.com>
2015-04-29 22:14:53 -0700

    Replace all code with a Tree a b data type
    I'd like to get a more all-purpose binary tree going here, which can
    store one type at its branches, and another at its leaves/tips.

--- 212_intermediate_animalGuessGame/Main.hs
+++ 212_intermediate_animalGuessGame/Main.hs
@@ -1,8 +1,2 @@
-data Qs = E | Q String Qs Qs | A String deriving (Show)
-data Next = Narrow | Guess | Fail
-
-next :: Qs -> String
-next E = "You've stumped me!"
-next (A s) = "My guess: " ++ s ++ "!"
-next (Q s y n) = s
+data Tree a b = E | B a (Tree a b) (Tree a b) | T b deriving (Show)
 
