00e9604709dfa93c8aafbdf09c9c955d18f2829b
gfixler <gary@garyfixler.com>
2015-04-30 00:53:35 -0700

    Add TreeZipper a b data type

--- 212_intermediate_animalGuessGame/Main.hs
+++ 212_intermediate_animalGuessGame/Main.hs
@@ -3,4 +3,10 @@ data Tree a b = Branch a (Tree a b) (Tree a b)
               | Leaf b
               deriving (Show)
 
+data TreeZipper a b = Top (Tree a b)
+                    | L (Tree a b) a (Tree a b)
+                    | R (Tree a b) a (Tree a b)
+                    | Bottom (Tree a b) a (Tree a b)
+                    deriving (Show)
+
 
