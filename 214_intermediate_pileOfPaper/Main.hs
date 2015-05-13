02c8dff1e95a60e45ff2223a9f09d86c42752718
gfixler <gary@garyfixler.com>
2015-05-12 23:57:33 -0700

    Add newPile function

--- 214_intermediate_pileOfPaper/Main.hs
+++ 214_intermediate_pileOfPaper/Main.hs
@@ -4,3 +4,6 @@ type Pile   = (Int, Int, Int, Int, [Sheet]) -- l, t, r, b, sheets
 onSheet :: Int -> Int -> Sheet -> Bool
 onSheet x y (_,l,t,w,h) = x >= l && y >= t && x <= l+w && y <= t+h
 
+newPile :: Int -> Int -> Pile
+newPile w h = (0,0,w,h,[])
+
