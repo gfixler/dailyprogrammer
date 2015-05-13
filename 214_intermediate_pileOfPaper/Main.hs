ba88f4076bfb723ed512a578ccd3a9c554597112
gfixler <gary@garyfixler.com>
2015-05-12 23:57:16 -0700

    Add Pile type to serve as the canvas

--- 214_intermediate_pileOfPaper/Main.hs
+++ 214_intermediate_pileOfPaper/Main.hs
@@ -1,4 +1,5 @@
-type Sheet = (Int, Int, Int, Int, Int) -- color, x, y, w, h
+type Sheet  = (Int, Int, Int, Int, Int) -- color, x, y, w, h
+type Pile   = (Int, Int, Int, Int, [Sheet]) -- l, t, r, b, sheets
 
 onSheet :: Int -> Int -> Sheet -> Bool
 onSheet x y (_,l,t,w,h) = x >= l && y >= t && x <= l+w && y <= t+h
