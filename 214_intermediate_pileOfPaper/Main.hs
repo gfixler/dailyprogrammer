d60c08112e0b2b53e44931169c1f9644b3aa0f2c
gfixler <gary@garyfixler.com>
2015-05-13 00:38:02 -0700

    Swap parameter order in onSheet

--- 214_intermediate_pileOfPaper/Main.hs
+++ 214_intermediate_pileOfPaper/Main.hs
@@ -1,8 +1,8 @@
 type Sheet  = (Int, Int, Int, Int, Int) -- color, x, y, w, h
 type Pile   = (Int, Int, Int, Int, [Sheet]) -- l, t, r, b, sheets
 
-onSheet :: Int -> Int -> Sheet -> Bool
-onSheet x y (_,l,t,w,h) = x >= l && y >= t && x <= l+w && y <= t+h
+onSheet :: Sheet -> Int -> Int -> Bool
+onSheet (_,l,t,w,h) x y = x >= l && y >= t && x <= l+w-1 && y <= t+h-1
 
 newPile :: Int -> Int -> Pile
 newPile w h = (0,0,w,h,[])
