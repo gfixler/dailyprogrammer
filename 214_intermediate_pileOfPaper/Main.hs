699e281dfc633f7c276ce533ba60fb834a933911
gfixler <gary@garyfixler.com>
2015-05-12 23:57:48 -0700

    Add pileOn function

--- 214_intermediate_pileOfPaper/Main.hs
+++ 214_intermediate_pileOfPaper/Main.hs
@@ -7,3 +7,7 @@ onSheet x y (_,l,t,w,h) = x >= l && y >= t && x <= l+w && y <= t+h
 newPile :: Int -> Int -> Pile
 newPile w h = (0,0,w,h,[])
 
+pileOn :: Sheet -> Pile -> Pile
+pileOn s@(c,x,y,w,h) (l,t,r,b,p) =
+        (min x l, min y t, max (x+w) r, max (y+h) b, s:p)
+
