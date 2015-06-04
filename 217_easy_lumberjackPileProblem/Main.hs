d74eeb9a094ecf8dd4ce76a904e2de8ca61cdbe2
gfixler <gary@garyfixler.com>
2015-06-04 12:16:53 -0700

    Flesh out main function; echo inputs back to user

--- 217_easy_lumberjackPileProblem/Main.hs
+++ 217_easy_lumberjackPileProblem/Main.hs
@@ -1,5 +1,14 @@
+import System.IO (getContents)
+
 readLinesOfInts :: String -> [Int]
 readLinesOfInts = concat . map (map read . words) . lines
 
-main = undefined
+-- example usage: cat input | runhaskell Main.hs
+main :: IO ()
+main = do
+    (d:n:ps) <- fmap readLinesOfInts getContents
+    print d
+    print n
+    print ps
+    return ()
 
