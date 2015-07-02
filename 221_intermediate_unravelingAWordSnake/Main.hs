f9ba05e0cadf086883fc4c166229ffb2e2dd97d8
gfixler <gary@garyfixler.com>
2015-07-01 18:20:00 -0700

    WIP on 221 Intermediate

--- 221_intermediate_unravelingAWordSnake/Main.hs
+++ 221_intermediate_unravelingAWordSnake/Main.hs
@@ -15,3 +15,18 @@ toSnakeMap :: String -> SnakeMap
 toSnakeMap = M.fromList . nospaces . concatMap numLineChars . numLines
     where nospaces = filter ((/=' ') . snd)
 
+left, right :: (Int, Int) -> (Int, Int)
+left x = case x of (1,0) -> (0,1)
+                   (0,1) -> (-1,0)
+                   (-1,0) -> (0,-1)
+                   (0,-1) -> (1,0)
+
+right = left . left . left
+
+-- popSnakeWord :: SnakeMap -> (Int,Int) -> (String, (Int,Int), SnakeMap)
+-- popSnakeWord m (x,y) = map (flip M.lookup m)
+
+main = print (toSnakeMap example)
+
+example = "SHENANIGANS       DOUBLET\n          A       N     E\n          L       U     R\n          T       O     A\n          YOUNGSTER     B\n                        Y\n                        T\n                  ECNESSE\n"
+
