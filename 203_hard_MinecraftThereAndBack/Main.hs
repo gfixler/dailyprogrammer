085eb0a5ccf30643d0bd1e174d97865427b1e035
gfixler <gary@garyfixler.com>
2015-08-26 23:49:02 -0700

    WIP - not sure what this old playings-about is

--- 203_hard_MinecraftThereAndBack/Main.hs
+++ 203_hard_MinecraftThereAndBack/Main.hs
@@ -5,23 +5,30 @@ import qualified Data.Map as M
 
 type Cell = (Int, Int, Int)
 data Block = Air | Dirt | Sand | Lava deriving (Eq, Show)
-type Core = [(Cell,Block)]
-type World = M.Map Cell Block
+type World = (Cell, M.Map Cell Block)
 
 genCells :: Cell -> [Cell]
 genCells (l,w,h) = [(x,y,z) | x <- [0..l-1], y <- [0..w-1], z <- [0..h-1]]
 
 newWorld :: Cell -> World
-newWorld cell = M.fromList $ zip (genCells cell) (repeat Air)
+newWorld cell = (cell, M.fromList $ zip (genCells cell) (repeat Air))
 
 worldSize :: World -> Cell
-worldSize w = last $ M.keys w
+worldSize = fst
 
 getBlock :: World -> Cell -> Maybe Block
-getBlock cells cell = M.lookup cell cells
+getBlock cells cell = M.lookup cell (snd cells)
 
 setBlock :: World -> Cell -> Block -> World
-setBlock w c b = M.adjust (const b) c w
+setBlock (s,w) c b = (s, M.adjust (const b) c w)
+
+genCore :: Cell -> [Cell]
+genCore (x,y,top) = [(x,y,z) | z <- [0..top]]
+
+genCores :: World -> M.Map (Int,Int) [Cell]
+genCores w = M.fromList [((x,y),genCore (x,y,top))
+                        | x <- [0..x'], y <- [0..y']]
+    where (x',y',top) = worldSize w
 
 collapse :: [Block] -> [Block] -- [bottom..top] ordering
 collapse xs = concat $ reorder $ group xs
@@ -30,9 +37,6 @@ collapse xs = concat $ reorder $ group xs
           reorder (x:xs)                    = x : reorder xs
           reorder []                        = []
 
-sampleCore :: World -> Int -> Int -> Maybe Core
-sampleCore w x y = fmap (zip core) sample
-    where (_,_,top) = worldSize w
-          core      = [(x,y,z) | z <- [top,top-1..0]]
-          sample    = sequence $ map (getBlock w) $ core
+-- sampleCore :: World -> Int -> Int -> Maybe [Block]
+-- sampleCore w x y = sequence $ map (getBlock w) $ core
 
