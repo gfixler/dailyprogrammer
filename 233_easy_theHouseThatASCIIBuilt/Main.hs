a8cb2741e041ad967035e366041251dddae7c68e
gfixler <gary@garyfixler.com>
2015-09-21 16:17:32 -0700

    Generalize CharPad to Grid a
    This will let me work not only with a grid of characters, but, e.g. (and
    my current impetus for the change), with grids of ADT constructors.

--- 233_easy_theHouseThatASCIIBuilt/Main.hs
+++ 233_easy_theHouseThatASCIIBuilt/Main.hs
@@ -1,6 +1,6 @@
 import qualified Data.Map as M (Map, fromList, keys, findWithDefault)
 
-type CharPad = M.Map (Int, Int) Char
+type Grid a = M.Map (Int, Int) a
 
 enum :: [a] -> [(Int, a)]
 enum = zip [0..]
@@ -14,12 +14,12 @@ numLineChars (y, s) = map (\(x,c) -> ((x,y),c)) (enum s)
 charGrid :: String -> [((Int, Int), Char)]
 charGrid = concatMap numLineChars . numLines
 
-charGridBounds :: CharPad -> ((Int, Int), (Int, Int))
+charGridBounds :: Grid Char -> ((Int, Int), (Int, Int))
 charGridBounds = foldr f e . M.keys
     where e = ((maxBound, maxBound), (minBound, minBound))
           f = \(x, y) ((xl, yl), (xh, yh)) -> ((min x xl, min y yl), (max x xh, max y yh))
 
-charAtCoord :: (Int, Int) -> CharPad -> Char
+charAtCoord :: (Int, Int) -> Grid Char -> Char
 charAtCoord = M.findWithDefault ' '
 
 renderCharPad p = unlines [[charAtCoord (x, y) p | y <- [yl .. yh]] | x <- [xl .. xh]]
