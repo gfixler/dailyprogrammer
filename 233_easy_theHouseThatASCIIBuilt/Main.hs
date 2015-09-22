07f493ff94c08e3c132b16d1512e276997e04eef
gfixler <gary@garyfixler.com>
2015-09-21 17:07:49 -0700

    Genericize point-getting function and defaulting

--- 233_easy_theHouseThatASCIIBuilt/Main.hs
+++ 233_easy_theHouseThatASCIIBuilt/Main.hs
@@ -20,9 +20,9 @@ gridBounds = foldr f e . M.keys
     where e = ((maxBound, maxBound), (minBound, minBound))
           f = \(x, y) ((xl, yl), (xh, yh)) -> ((min x xl, min y yl), (max x xh, max y yh))
 
-charAtCoord :: Coord -> Grid Char -> Char
-charAtCoord = M.findWithDefault ' '
+gridPtOr :: a -> Coord -> Grid a -> a
+gridPtOr = M.findWithDefault
 
-renderCharPad p = unlines [[charAtCoord (x, y) p | y <- [yl .. yh]] | x <- [xl .. xh]]
+renderCharPad p = unlines [[gridPtOr ' ' (x, y) p | y <- [yl .. yh]] | x <- [xl .. xh]]
     where ((xl, yl), (xh, yh)) = gridBounds p
 
