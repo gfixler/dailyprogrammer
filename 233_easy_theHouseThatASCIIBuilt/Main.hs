fc58b015ed2b277301d4947f860c01abed862f4a
gfixler <gary@garyfixler.com>
2015-09-21 17:01:44 -0700

    Add Coord datatype, replace all (Int, Int)

--- 233_easy_theHouseThatASCIIBuilt/Main.hs
+++ 233_easy_theHouseThatASCIIBuilt/Main.hs
@@ -1,6 +1,7 @@
 import qualified Data.Map as M (Map, fromList, keys, findWithDefault)
 
-type Grid a = M.Map (Int, Int) a
+type Coord = (Int, Int)
+type Grid a = M.Map Coord a
 
 enum :: [a] -> [(Int, a)]
 enum = zip [0..]
@@ -8,18 +9,18 @@ enum = zip [0..]
 toYLines :: String -> [(Int, String)]
 toYLines = reverse . enum . reverse . lines
 
-yLineToXChars :: (Int, String) -> [((Int, Int), Char)]
+yLineToXChars :: (Int, String) -> [(Coord, Char)]
 yLineToXChars (y, s) = map (\(x,c) -> ((x,y),c)) (enum s)
 
-charGrid :: String -> [((Int, Int), Char)]
+charGrid :: String -> [(Coord, Char)]
 charGrid = concatMap yLineToXChars . toYLines
 
-charGridBounds :: Grid Char -> ((Int, Int), (Int, Int))
+charGridBounds :: Grid Char -> (Coord, Coord)
 charGridBounds = foldr f e . M.keys
     where e = ((maxBound, maxBound), (minBound, minBound))
           f = \(x, y) ((xl, yl), (xh, yh)) -> ((min x xl, min y yl), (max x xh, max y yh))
 
-charAtCoord :: (Int, Int) -> Grid Char -> Char
+charAtCoord :: Coord -> Grid Char -> Char
 charAtCoord = M.findWithDefault ' '
 
 renderCharPad p = unlines [[charAtCoord (x, y) p | y <- [yl .. yh]] | x <- [xl .. xh]]
