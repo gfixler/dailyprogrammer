11bea5e16c967870bf8976af9dccbd9c5e47f92a
gfixler <gary@garyfixler.com>
2015-09-21 18:15:51 -0700

    Break out char grid stuff to new CharGrid.hs

---
+++ 233_easy_theHouseThatASCIIBuilt/CharGrid.hs
@@ -0,0 +1,27 @@
+module CharGrid where
+
+import qualified Data.Map as M (fromList)
+
+import Grid
+
+enum :: [a] -> [(Int, a)]
+enum = zip [0..]
+
+-- split string into lines enumerated with height values
+toYLines :: String -> [(Int, String)]
+toYLines = reverse . enum . reverse . lines
+
+-- split row-enumerated line into (x,y) tagged Char list
+yLineToXChars :: (Int, String) -> [(Coord, Char)]
+yLineToXChars (y, s) = map (\(x,c) -> ((x,y),c)) (enum s)
+
+-- split string into (x,y)-enumerated Chars
+toCharGrid :: String -> Grid Char
+toCharGrid = M.fromList . concatMap yLineToXChars . toYLines
+
+-- assemble bbox-sized, "2D" string from Grid of Chars
+renderCharGrid :: Grid Char -> String
+renderCharGrid p = unlines [[gridPtOr ' ' (x, y) p | y <- [yl .. yh]] | x <- [xl .. xh]]
+    where ((xl, yl), (xh, yh)) = gridBounds p
+
+
--- 233_easy_theHouseThatASCIIBuilt/Main.hs
+++ 233_easy_theHouseThatASCIIBuilt/Main.hs
@@ -1,24 +1,6 @@
 module Main where
 
-import Grid
+import System.IO (getContents)
 
-enum :: [a] -> [(Int, a)]
-enum = zip [0..]
-
--- split string into lines enumerated with height values
-toYLines :: String -> [(Int, String)]
-toYLines = reverse . enum . reverse . lines
-
--- split row-enumerated line into (x,y) tagged Char list
-yLineToXChars :: (Int, String) -> [(Coord, Char)]
-yLineToXChars (y, s) = map (\(x,c) -> ((x,y),c)) (enum s)
-
--- split string into (x,y)-enumerated Chars
-toGrid :: String -> [(Coord, Char)]
-toGrid = concatMap yLineToXChars . toYLines
-
--- assemble bbox-sized, "2D" string from Grid of Chars
-renderCharPad :: Grid Char -> String
-renderCharPad p = unlines [[gridPtOr ' ' (x, y) p | y <- [yl .. yh]] | x <- [xl .. xh]]
-    where ((xl, yl), (xh, yh)) = gridBounds p
+import CharGrid
 
