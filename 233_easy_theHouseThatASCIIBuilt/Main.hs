bda1edf5c488469d4125849613ae1b9036116d6f
gfixler <gary@garyfixler.com>
2015-09-21 17:05:47 -0700

    Genericize grid creation/bounds-checking functions

--- 233_easy_theHouseThatASCIIBuilt/Main.hs
+++ 233_easy_theHouseThatASCIIBuilt/Main.hs
@@ -12,11 +12,11 @@ toYLines = reverse . enum . reverse . lines
 yLineToXChars :: (Int, String) -> [(Coord, Char)]
 yLineToXChars (y, s) = map (\(x,c) -> ((x,y),c)) (enum s)
 
-charGrid :: String -> [(Coord, Char)]
-charGrid = concatMap yLineToXChars . toYLines
+toGrid :: String -> [(Coord, Char)]
+toGrid = concatMap yLineToXChars . toYLines
 
-charGridBounds :: Grid Char -> (Coord, Coord)
-charGridBounds = foldr f e . M.keys
+gridBounds :: Grid a -> (Coord, Coord)
+gridBounds = foldr f e . M.keys
     where e = ((maxBound, maxBound), (minBound, minBound))
           f = \(x, y) ((xl, yl), (xh, yh)) -> ((min x xl, min y yl), (max x xh, max y yh))
 
@@ -24,5 +24,5 @@ charAtCoord :: Coord -> Grid Char -> Char
 charAtCoord = M.findWithDefault ' '
 
 renderCharPad p = unlines [[charAtCoord (x, y) p | y <- [yl .. yh]] | x <- [xl .. xh]]
-    where ((xl, yl), (xh, yh)) = charGridBounds p
+    where ((xl, yl), (xh, yh)) = gridBounds p
 
