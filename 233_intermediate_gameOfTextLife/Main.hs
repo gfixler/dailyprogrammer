318d56514472cd63d2adf0ebd2fabd6b7bd45f3a
gfixler <gary@garyfixler.com>
2015-09-24 21:09:06 -0700

    Add CharGrid, Coord datatypes; use in stringrid

--- 233_intermediate_gameOfTextLife/Main.hs
+++ 233_intermediate_gameOfTextLife/Main.hs
@@ -2,8 +2,11 @@ module Main where
 
 import qualified Data.Map as M (Map, fromList)
 
+type CharGrid = M.Map (Int, Int) Char
+type Coord = (Int, Int)
+
 -- maps XY positions in [potentially multiline] String to their Chars
-stringrid :: String -> M.Map (Int, Int) Char
+stringrid :: String -> CharGrid
 stringrid = M.fromList . g (0, 0)
     where g :: (Int, Int) -> String -> [((Int, Int), Char)]
           g _ []             = []
