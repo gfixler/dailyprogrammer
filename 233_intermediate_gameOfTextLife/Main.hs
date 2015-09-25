3ab610bf89890053504a4d05fb9078ef934641e6
gfixler <gary@garyfixler.com>
2015-09-24 21:34:17 -0700

    Add bbox function

--- 233_intermediate_gameOfTextLife/Main.hs
+++ 233_intermediate_gameOfTextLife/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import qualified Data.Map as M (Map, fromList)
+import qualified Data.Map as M (Map, fromList, keys)
 
 type CharGrid = M.Map (Int, Int) Char
 type Coord = (Int, Int)
@@ -13,4 +13,15 @@ stringrid = M.fromList . g (0, 0)
           g (x, y) ('\n':cs) = g (0, succ y) cs
           g (x, y) (' ':cs)  = g (succ x, y) cs
           g (x, y) (c:cs)    = ((x, y), c) : g (succ x, y) cs
+
+-- finds lower-left and upper-right coords as a pair of pairs of Ints
+bbox :: CharGrid -> (Coord, Coord)
+bbox m = ((xl, yl), (xh, yh))
+    where ks = M.keys m
+          xs = map fst ks
+          ys = map snd ks
+          xl = minimum xs
+          yl = minimum ys
+          xh = maximum xs
+          yh = maximum ys
 
